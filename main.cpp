fc0842b28b0312337fa84f38e4e605740869d25e
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2023-04-28 12:34:29 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,420 +1,225 @@
 #include <iostream>
-#include <cstring>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Bus.h"
+#include "Truck.h"
+#include "VehicleType.h"
+#include "garage.h"
 
+#include <memory>
 using namespace std;
 
-class Car{
-private:
-    int fabrication_year;
-    char *model;
-    double price;
-    int speed_per_hour;
-    char *colour;
-    double fuel_consumption;
-    char *fuel_type;
-    int kilometres;
-
-public:
-    ///constructor fara parametri
-    Car()
-    {
-        ///cout<<"S-a apelat constructorul fara parametri "<<endl;
-        fabrication_year=0;
-        model=new char[strlen("Unknown")+1];
-        strcpy(model,"Unknown");
-        price=0;
-        speed_per_hour=0;
-        colour=new char[strlen("Unknown")+1];
-        strcpy(colour,"Unknown");
-        fuel_consumption=0;
-        fuel_type=new char[strlen("Unknown")+1];
-        strcpy(fuel_type,"Unknown");
-        kilometres=0;
-    }
-
-    /// constructor cu parametri
-    Car(int fabrication_year_, const char* model_, double price_, int speed_per_hour_,const char* colour_,double fuel_consumpion_, const char* fuel_type_, int kilometers_)
-    {
-       ///cout<<"S-a apelat constructorul cu parametri "<<endl;
-        fabrication_year=fabrication_year_;
-        size_t len = strlen(model_);
-        model = new char[len + 1];
-        strcpy(model, model_);
-        price=price_;
-        speed_per_hour=speed_per_hour_;
-        size_t len1 = strlen(colour_);
-        colour = new char[len1 + 1];
-        strcpy(colour, colour_);
-        fuel_consumption=fuel_consumpion_;
-        size_t len2 = strlen(fuel_type_);
-        fuel_type = new char[len2 + 1];
-        strcpy(fuel_type, fuel_type_);
-        kilometres=kilometers_;
-    }
-    ///Destructori
-    ~Car() {
-        delete[] model;
-        delete[] colour;
-        delete[] fuel_type;
-    }
-
-    Car(const Car &ob)  ///copy constructor
-    {
-       /// cout<<"S-a apelat constructorul de copiere "<<endl;
-        fabrication_year=ob.fabrication_year;
-        size_t len = strlen(ob.model);
-        model = new char[len + 1];
-        strcpy(model, ob.model);
-        price=ob.price;
-        speed_per_hour=ob.speed_per_hour;
-        size_t len1 = strlen(ob.colour);
-        colour = new char[len1 + 1];
-        strcpy(colour, ob.colour);
-        fuel_consumption=ob.fuel_consumption;
-        size_t len2 = strlen(ob.fuel_type);
-        fuel_type = new char[len2 + 1];
-        strcpy(fuel_type, ob.fuel_type);
-        kilometres=ob.kilometres;
-
-    }
-
-    void afisare()
-    {
-        cout<<"Year: "<<fabrication_year<<endl;
-        cout<<"Model: "<<model<<endl;
-        cout<<"Colour: "<<colour<<endl;
-        cout<<"Fuel consumption: "<<fuel_consumption<<endl;
-        cout<<"Fuel type: "<<fuel_type<<endl;
-        cout<<"Price: "<<price<<endl;
-        cout<<"Kilometres: "<<kilometres<<endl;
-        cout<<"Speed per hour: "<<speed_per_hour<<endl;
-    }
-
-    void setFabricationYear(const int fabrication_year_)
-    {
-        fabrication_year=fabrication_year_;
-    }
-    void setModel(const char* model_)
-    {
-        delete[] model;
-        size_t len=strlen(model_);
-        model=new char[len+1];
-        strcpy(model,model_);
-    }
-    void setPrice(const double price_)
-    {
-        price=price_;
-    }
-    void setSpeed(const int speed_)
-    {
-        speed_per_hour=speed_;
-    }
-
-    void setColour(const char* colour_)
-    {
-        delete[] colour;
-        size_t len=strlen(colour_);
-        colour=new char[len+1];
-        strcpy(colour,colour_);
-    }
-    void setFuelConsumption(const double fuel_consumpion_)
-    {
-        fuel_consumption=fuel_consumpion_;
-    }
-    void setFuelType(const char* fuel_type_)
-    {
-        delete[] fuel_type;
-        size_t len=strlen(fuel_type_);
-        fuel_type=new char[len+1];
-        strcpy(fuel_type,fuel_type_);
-    }
-    void setKilometres(const int kilometres_)
-    {
-        kilometres=kilometres_;
-    }
-    int getFabrication_year() const
-    {
-        return fabrication_year;
-    }
-    char* getModel() const
-    {
-        return model;
-    }
-    double getPrice() const
-    {
-        return price;
-    }
-    int getSpeedPerHour() const
-    {
-        return speed_per_hour;
-    }
-    char* getColour() const
-    {
-        return colour;
-    }
-    double getFuelConsumpion() const
-    {
-        return fuel_consumption;
-    }
-    char* getFuelType() const
-    {
-        return fuel_type;
-    }
-    int getKilometers() const
-    {
-        return kilometres;
-    }
-
-    ///rescrierea operatorilor
-    /*--forma
-     TipReturn operator+(const TipData2 &rhs) const {
-    ...
-    }
-     */
-
-    ///operatorul =
-    Car& operator =(const Car &rhs)
-    {
-
-        if (this != &rhs){
-        cout<<"S-a apelat operatorul ="<<endl;
-        this->fabrication_year=rhs.fabrication_year;
-        if (this != &rhs)setModel(rhs.model);
-        this->price=rhs.price;
-        this->speed_per_hour=rhs.speed_per_hour;
-        if (this != &rhs) setColour(rhs.colour);
-        this->fuel_consumption=rhs.fuel_consumption;
-        if (this != &rhs) setFuelType(rhs.fuel_type);
-        this->kilometres=rhs.kilometres;}
-
-        return *this;
-    }
-
-    ///Operatorii == si !=
-
-    bool operator ==(const Car &c) const
-    {
-        return fabrication_year==c.fabrication_year && strcmp(model,c.getModel())==0 && price==c.price
-        && speed_per_hour==c.speed_per_hour && strcmp(colour,c.getColour())==0 && fuel_consumption==c.fuel_consumption
-        && strcmp(fuel_type,c.getFuelType())==0 && kilometres==c.kilometres;
-    }
-
-    bool operator !=(const Car &c) const
-    {
-        return fabrication_year!=c.fabrication_year || strcmp(model,c.getModel())!=0 || price!=c.price
-               || speed_per_hour!=c.speed_per_hour || strcmp(colour,c.colour)!=0 || fuel_consumption!=c.fuel_consumption
-               || strcmp(fuel_type,c.getFuelType())!=0 || kilometres!=c.kilometres;
-    }
-
-
-    ///Supraincarcarea operatorilor << si >>
-
-    friend istream& operator>>(istream& in, Car& c1);
-    friend ostream& operator<<(ostream& out, const Car& c1);
-
-
-};
-
-istream& operator>>(istream& in, Car& c1)
-{
-    int y;
-    in>>y;
-    c1.setFabricationYear(y);
-    char m[100];
-    in>>m;
-    c1.setModel(m);
-    double x;
-    in>>x;
-    c1.setPrice(x);
-    int s;
-    in>>s;
-    c1.setSpeed(s);
-    char c[100];
-    in>>c;
-    c1.setColour(c);
-    double f;
-    in>>f;
-    c1.setFuelConsumption(f);
-    char ft[100];
-    in>>ft;
-    c1.setFuelType(ft);
-    int k;
-    in>>k;
-    c1.setKilometres(k);
-    /*in>>c1.fabrication_year>> c1.getModel()>> c1.price>>c1.speed_per_hour>>
-    c1.getColour()>>c1.fuel_consumption>>c1.getFuelType()>>c1.kilometres;*/
-    return in;
-}
-
-ostream& operator<<(ostream& out, const Car& c1) {
-    out<<c1.fabrication_year<<" "<< c1.getModel()<< " " << c1.price << " " <<c1.speed_per_hour<<
-    " "<<" "<< c1.getColour()<<" "<<c1.fuel_consumption<<" "<<c1.getFuelType()<<" "<<c1.kilometres<<endl;
-    return out;
-}
-
-class Race
-{
-private:
-    int year_of_fabrication;
-    int speed_min;
-    char *fuel_type2;
-
-public:
-    ///constructor fara parametri
-    Race()
-    {
-        cout<<"S-a apelat constructorul fara parametri "<<endl;
-        year_of_fabrication=0;
-        speed_min=0;
-        fuel_type2=new char[strlen("Unknown")+1];
-        strcpy(fuel_type2,"Unknown");
-    }
-
-    /// constructor cu parametri
-    Race(int year_of_fabrication_, int speed_min_,const char* fuel_type2_)
-    {
-        cout<<"S-a apelat constructorul cu parametri "<<endl;
-        year_of_fabrication=year_of_fabrication_;
-        speed_min=speed_min_;
-        size_t len2 = strlen(fuel_type2_);
-        fuel_type2 = new char[len2 + 1];
-        strcpy(fuel_type2, fuel_type2_);
-
-    }
-    ///Destructori
-    ~Race() {
-        delete[] fuel_type2;
-    }
-
-    Race(const Race &ob)  ///copy constructor
-    {
-        cout<<"S-a apelat constructorul de copiere "<<endl;
-        year_of_fabrication=ob.year_of_fabrication;
-        speed_min=ob.speed_min;
-        size_t len2 = strlen(ob.fuel_type2);
-        fuel_type2 = new char[len2 + 1];
-        strcpy(fuel_type2, ob.fuel_type2);
-    }
-    void setYear(const double year_)
-    {
-        year_of_fabrication=year_;
-    }
-    void setSpeed(const int speed2_)
-    {
-        speed_min=speed2_;
-    }
-
-    void setFuelType2(const char* fuel_type2_)
-    {
-        size_t len=strlen(fuel_type2_);
-        fuel_type2=new char[len+1];
-        strcpy(fuel_type2,fuel_type2_);
-    }
-
-    int getYear() const
-    {
-        return year_of_fabrication;
-    }
-    int getSpeedMin() const
-    {
-        return speed_min;
-    }
-    const char* getFuelType2() const
-    {
-        return fuel_type2;
-    }
-    Race& operator =(const Race &rhs)
-    {
-        delete[] fuel_type2;
-        if (this != &rhs) {
-            cout << "S-a apelat operatorul =" << endl;
-            this->year_of_fabrication = rhs.year_of_fabrication;
-            if (this != &rhs)setFuelType2(rhs.fuel_type2);
-            this->speed_min = rhs.speed_min;
-        }
-
-        return *this;
-    }
-};
-
-
-void cursa()
-{
-    Car m1(2012,"BMW",12000,300,"blue",7.3,"diesel",120000);
-    Race r1(2020,250,"diesel");
-    if (m1.getFabrication_year()>=r1.getYear()&&m1.getSpeedPerHour()>=r1.getSpeedMin()&&strcmp(m1.getFuelType(),r1.getFuelType2())==0)cout<<"Masina poate participa la cursa"<<endl;
-    else cout<<"Masina nu detine dotarile necesare"<<endl;
-}
-
-void cumparare()
-{
-    Car m1(2012,"BMW",12000,300,"blue",7.3,"diesel",120000);
-    cout<<"Cerintele cumparatorului sunt: "<<endl;
-    cout<<"1. Masina sa nu aiba anul de aparitie inainte de 2017"<<endl<<"2.Masina sa functioneze cu motorina"<<endl
-        <<"3.Masina sa nu depaseasca 20000 euro"<<endl<<"4.Masina sa fie neagra"<<endl;
-    if(m1.getFabrication_year()>=2017&&strcmp(m1.getFuelType(),"diesel")==0&&m1.getPrice()<=20000&&strcmp(m1.getColour(),"negru")==0)
-        cout<<"Masina respecta cerintele cumparatorului si aceasta o poate cumpara"<<endl;
-    else
-        cout<<"Masina nu este ceea ce cumparatorul isi doreste"<<endl<<endl;
-}
-
 int main() {
+    /* Car c(2012, "BMW", 12000,"blue",7.3,"diesel",20000, true, false);
+     Car c2(1000,"dfhg",3453,"red",7.3,"diesel",20000,true, true);
 
-    Car c1(2012,"BMW",12000,300,"blue",7.3,"diesel",120000);
-    c1.afisare();
-    cout<<endl;
-    Car c2(c1);
-    c2.afisare();
-    cout<<endl;
-    c2.setFabricationYear(2020);
-    c2.setModel("Fiat 500");
-    c2.setPrice(15000);
-    c2.setSpeed(240);
-    c2.setColour("white");
-    c2.setFuelConsumption(8.2);
-    c2.setFuelType("gasoline");
-    c2.setKilometres(75000);
-    cout<<c2.getFabrication_year();
-    c2.afisare();
-    cout<<endl;
-    cout<<" "<<c2.getModel();
-    cout<<" "<<c2.getColour();
-    cout<<" "<<c2.getFuelConsumpion();
-    cout<<" "<<c2.getFuelType();
-    cout<<" "<<c2.getKilometers();
-    cout<<" "<<c2.getPrice();
-    cout<<" "<<c2.getSpeedPerHour()<<endl;
-    Car c3(2010,"Audi",8000,280,"black",6.8,"diesel",280000);
-    c3.afisare();
-    cout<<endl;
-    c3=c1;
-    c3.afisare();
-    cout<<endl;
-    Car c4;
-    c4.afisare();
-    cout<<endl;
-    c4=c2;
-    c4.afisare();cout<<endl;
-    cout<<(c4==c3)<<endl;
-    c4=c3;
+   Vehicle* vehicul=new Car(2012, "BMW", 12000,"blue",7.3,"diesel",20000, false );
+     vehicul->afisareDotari();
+     delete vehicul;
 
-    Race r1(2020,300,"diesel");
-    r1.getFuelType2();
-    r1.getSpeedMin();
-    r1.getYear();
-    Race r2;
-    r2.setSpeed(370);
-    r2.setFuelType2("gasoline");
-    r2.setYear(2022);
+     c=c2;
+     c.afisareDotari();
+     cout<<c.getCondition();
+     Bus b(2012, "BMW", 12000,"blue",30,200,7,3,11.3, true);
+     Bus b2(20122, "BcdsMW", 123000,"bdvlue",10,400,5,2,13.3, false);
+     b=b2;
+     b.setSituation(true);
+     cout<<b;
+     cout<<b.verificaStarea();
+     Vehicle *ob=new Car();
+     cout<<*ob;*/
 
-    int x;
-    x=1;
-    do {
-        cout<<"1.Verificam daca masina poate participa la cursa"<<endl;
-        cout<<"2.Verificam daca masina se inadreaza in cerintele unui cumparator"<<endl;
-        if(x==1)
-        {cursa(); x=2;}
-        if(x==2)
-        {cumparare();x=3;}
-    }while(x==1 || x==2);
+    int nr_vehicles=15, nr_cars=7, nr_buses=5, nr_trucks=3,garageCapacity=15;
+    auto c1=make_shared<Car>(2020, "AUDI", 23000, "black", 8.7, "diesel", 40000, true, false);
+    auto c2= make_shared<Car>(2010, "BMW",6000,"gray",9.7,"gasoline",140000, false, false);
+    auto c3= make_shared<Car>(2022, "PORCHE",50000,"purple",11.3,"diesel",1000,true, false);
+    auto c4= make_shared<Car>(2004, "DACIA",1000,"black",4.5,"gas",340000, false, true);
+    auto c5= make_shared<Car>(2014, "Fiat 500",10000,"blue",8.1,"diesel",45000,true, false);
+    auto c6= make_shared<Car>(2017, "OPEL",11000,"light blue",6.3,"gasoline",40000,true, true);
+    auto c7= make_shared<Car>(2007, "VOLKSWAGEN",4500,"black",6.9,"diesel",240000, false, false);
+
+    auto b1= make_shared<Bus>(2012, "MERCEDESS",12200, "black",45,210,6,3,10.2,false);
+    auto b2= make_shared<Bus>(2005, "MERCEDESS",7500, "white",27,170,4,2,9.4,true);
+    auto b3= make_shared<Bus>(2017, "MERCEDESS",13500, "gray",40,220,5,3,10.2,true);
+    auto b4= make_shared<Bus>(2020, "MERCEDESS",16200, "black",60,210,6,3,13.2,false);
+    auto b5= make_shared<Bus>(2009, "MERCEDESS",8200, "white",35,190,4,2,9.1, true);
+
+    auto t1= make_shared<Truck>(2016,"KLEYN",30000,"red",true,5,4.5,2);
+    auto t2= make_shared<Truck>(2021,"RENAULT",50000,"white",false, 3,5,1);
+    auto t3= make_shared<Truck>(2009,"SCANIA",15000,"black", false,6,4.8,3);
+
+
+  /*  Vehicle* v=new Car(2020, "AUDI", 23000, "black", 8.7, "diesel", 40000, true, false);
+    Car* c=dynamic_cast<Car*>(v);
+    if (c != nullptr) {
+        cout<<"conversia a reusit\n";c->afisareDotari();
+    } else {
+        cout<<"conversia nu a reusit";
+    }*/
+
+    try {
+    Garage::AddVehicle(c1); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(c2); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(c3); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+        Garage::AddVehicle(c4); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(c5); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(c6); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(c7); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    try{
+    Garage::AddVehicle(b1); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    try{
+        Garage::AddVehicle(b2); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(b3); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(b4); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(b5); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    try{
+    Garage::AddVehicle(t1); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(t2); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    try{
+    Garage::AddVehicle(t3); }
+    catch (const VehicleAlreadyExistsException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+
+    int current_nr=9;
+   /* Garage::ShowAllVehicles();
+    Garage::ShowAllBrokenCars();
+    Garage::ShowAllBuses();*/
+
+    start:
+    cout<<"Bine ati venit!\n Acesta este un garaj destinat masinilor, autobuzelor si camioanelor.\nIn garaj putem intorduce doar masinile intr-o stare de functionare buna"
+         << ", autobuzele fara pasageri si camioanele fara marfa. \n Puteti accesa mai multe date ale garajului, precum si ce se afla in afara lui, cum ar fi:\n"
+            <<"1) Toate vehiculele din garaj\n2)Toate masinile din garaj\n3)Toate masinile stricate\n4)Autobuzele din garaj\n"
+              <<"5)Autobuzele care se afla pe traseu\n6)Camioanele din garaj\n7)Camioanele cu marfa\n8)Adaugati propria masina\n";
+
+    int n;
+    cout<<"Introduceti optiunea:\n";
+    cin>>n;
+    switch (n) {
+        case 1:
+        {cout<<"Vehiculele care se afla in garaj sunt in numar de: "<<current_nr<<"\n\n";
+            Garage::ShowAllVehicles();
+            break;}
+        case 2:
+        { cout<<"Masinile care se afla in garaj sunt:\n\n";
+            Garage::ShowAllCars();
+            break;}
+        case 3:
+        {
+            cout<<"Masinile stricate sunt:\n\n";
+            Garage::ShowAllBrokenCars();
+            break;
+        }
+        case 4:
+        {
+            cout<<"Autobuzele din garaj sunt:\n\n";
+            Garage::ShowAllBuses();
+            break;
+        }
+        case 5:
+        {
+            cout<<"Autobuzele care se afla pe traseu sunt:\n\n";
+            Garage::ShowOnRoadBuses();
+            break;
+        }
+        case 6:
+        {
+            cout<<"Camioanele din garaj sunt: \n";
+            Garage::ShowAllTrucks();
+            break;
+        }
+        case 7:
+        {
+            cout<<"Camioanele incarcate sunt: \n";
+            Garage::ShowLoadedTrucks();
+            break;
+        }
+        case 8:
+        {
+            if(current_nr<garageCapacity)
+            {
+                Car* c = Garage::AddOwnCar(shared_ptr<Car>());
+                if (c != nullptr) {
+                    current_nr++;
+                    auto newCar = make_shared<Car>(c->getFabrication_year(), c->getModel(), c->getPrice(), c->getColour(), c->getFuelConsumption(),
+                                                   c->getFuelType(), c->getKilometres(), c->getNav(), c->getCondition());
+
+                    Garage::AddVehicle(newCar);
+                cout<<"Masina dumneavoastra a fost adaugata cu succes. Va asteptam cat mai curand sa o aduceti la garajul nostru.\nDupa 2 zile rezervarea va fi amanata\n";}
+
+                else {cout<<"Datele introduse nu sunt corecte\n"; break;}
+            }
+            else cout<<"Nu mai exista loc in garaj\n";
+            break;
+        }
+        default:
+        {
+            cout<<"Multumim de vizionare! Va mai asteptam\n";
+            return 0;
+        }
+    }
+
+    cout<<"Doriti sa continuati sa vizualizati datele despre vehicule?\n1)DA\n2)NU\n";
+    int o;
+    cin>>o;
+    if(o==1)goto start;
+    else cout<<"Multumim de vizionare! Va mai asteptam\n";
+
+    return 0;
 }
